e656ac4a362312863fa8a4b22154bab6bcab2583
Devansh Batham <devanshbatham009@gmail.com>
2020-04-12 16:18:23 +0530

    Create extractor.py

---
+++ core/extractor.py
@@ -0,0 +1,31 @@
+import re 
+
+def param_extract(response , level , black_list ):
+
+    '''
+    regexp : r'.*?:\/\/.*\?.*\=[^$]'
+    regexp : r'.*?:\/\/.*\?.*\='
+    '''
+
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*\=[^$]' , response)))
+    final_uris = []
+        
+    for i in parsed:
+        delim = i.find('=')
+        second_delim = i.find('=', i.find('=') + 1)
+        if len(black_list) > 0:
+            words_re = re.compile("|".join(black_list))
+            if not words_re.search(i):
+                final_uris.append((i[:delim+1] + "FUZZ"))
+                if level == 'high':
+                    final_uris.append(i[:second_delim+1] + "FUZZ")
+        else:
+            final_uris.append((i[:delim+1] + "FUZZ"))
+            if level == 'high':
+                final_uris.append(i[:second_delim+1] + "FUZZ")
+
+    # for i in final_uris:
+    #     k = [ele for ele in black_list if(ele in i)]
+
+    
+    return list(set(final_uris))
