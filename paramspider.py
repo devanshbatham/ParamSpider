68fbbe863ced3c3bb927c030e155a26ffae9d2cd
Víctor Zamanillo <bellzebu@hotmail.com>
2020-05-05 21:07:36 +0200

    Missing code format

--- paramspider.py
+++ paramspider.py
@@ -34,7 +34,7 @@ def main():
     parser.add_argument('-d', '--domain',      help = 'Domain name of the taget [ex : hackerone.com]' , required = False)
     parser.add_argument('-s', '--subs',        help = 'Set False for no subs [ex : --subs False ]' , default = True)
     parser.add_argument('-l', '--level',       help = 'For nested parameters [ex : --level high]')
-    parser.add_argument('-e', '--exclude',     help= 'extensions to exclude [ex --exclude php,aspx]')
+    parser.add_argument('-e', '--exclude',     help = 'extensions to exclude [ex --exclude php,aspx]')
     parser.add_argument('-o', '--output',      help = 'Output file name [by default it is \'domain.txt\']')
     parser.add_argument('-p', '--placeholder', help = 'The string to add as a placeholder after the parameter name.', default = "FUZZ")
     parser.add_argument('-q', '--quiet',       help = 'Do not print the results to the screen', action = 'store_true')
