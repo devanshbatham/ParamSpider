5010487116f33ea8ea9d2fe69dfd9d7eae9baab6
Víctor Zamanillo <bellzebu@hotmail.com>
2020-05-05 20:23:45 +0200

    Changed domain param to generic filename param

--- core/save_it.py
+++ core/save_it.py
@@ -2,26 +2,26 @@ import os
 import errno
 
 
-def save_func(final_urls , outfile , domain):
+def save_func(final_urls , outfile , filename):
     if outfile:
         if "/" in outfile:
-            filename = f'{outfile}'
-        else : 
-            filename = f'output/{outfile}'
+            file_path = f'{outfile}'
+        else :
+            file_path = f'output/{outfile}'
     else :
-        filename = f"output/{domain}.txt"
-    
-    if os.path.exists(filename):
-        os.remove(filename)
+        file_path = f"output/{filename}.txt"
 
-    if not os.path.exists(os.path.dirname(filename)):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+    if not os.path.exists(os.path.dirname(file_path)):
         try:
-            os.makedirs(os.path.dirname(filename))
-        except OSError as exc: 
+            os.makedirs(os.path.dirname(file_path))
+        except OSError as exc:
             if exc.errno != errno.EEXIST:
                 raise
-    
-    
+
+
     for i in final_urls:
-        with open(filename, "a" , encoding="utf-8") as f:
+        with open(file_path, "a" , encoding="utf-8") as f:
             f.write(i+"\n")
