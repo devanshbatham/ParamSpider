1b5112aa0ebe74b81b68d66022a9771e59f05a71
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-06-01 14:38:59 +0300

    Format files using black

--- paramspider.py
+++ paramspider.py
@@ -2,19 +2,20 @@
 from core import requester
 from core import extractor
 from core import save_it
-from urllib.parse import unquote 
+from urllib.parse import unquote
 import argparse
 import os
-import time 
+import time
+
 start_time = time.time()
 
 
 def clear_screen():
     """Clears the screen"""
-    if os.name == 'nt': # for windows
-        os.system('cls')
-    else: # for linux
-        os.system('clear')
+    if os.name == "nt":  # for windows
+        os.system("cls")
+    else:  # for linux
+        os.system("clear")
 
 
 def print_banner():
@@ -34,15 +35,49 @@ def print_banner():
 
 def get_args():
     """Get arguments from the user"""
-    parser = argparse.ArgumentParser(description='ParamSpider a parameter discovery suite')
-    parser.add_argument('-d','--domain' , help = 'Domain name of the taget [ex : hackerone.com]' , required=True)
-    parser.add_argument('-s' ,'--subs' , help = 'Set False for no subs [ex : --subs False ]' , default='True', choices=['True','False'])
-    parser.add_argument('-l','--level' ,  help = 'For nested parameters [ex : --level high]')
-    parser.add_argument('-e','--exclude', help= 'extensions to exclude [ex --exclude php,aspx]')
-    parser.add_argument('-o','--output' , help = 'Output file name [by default it is \'domain.txt\']')
-    parser.add_argument('-p','--placeholder' , help = 'The string to add as a placeholder after the parameter name.', default = "FUZZ")
-    parser.add_argument('-q', '--quiet', help='Do not print the results to the screen', action='store_true')
-    parser.add_argument('-r', '--retries', help='Specify number of retries for 4xx and 5xx errors', default=3)
+    parser = argparse.ArgumentParser(
+        description="ParamSpider a parameter discovery suite"
+    )
+    parser.add_argument(
+        "-d",
+        "--domain",
+        help="Domain name of the taget [ex : hackerone.com]",
+        required=True,
+    )
+    parser.add_argument(
+        "-s",
+        "--subs",
+        help="Set False for no subs [ex : --subs False ]",
+        default="True",
+        choices=["True", "False"],
+    )
+    parser.add_argument(
+        "-l", "--level", help="For nested parameters [ex : --level high]"
+    )
+    parser.add_argument(
+        "-e", "--exclude", help="extensions to exclude [ex --exclude php,aspx]"
+    )
+    parser.add_argument(
+        "-o", "--output", help="Output file name [by default it is 'domain.txt']"
+    )
+    parser.add_argument(
+        "-p",
+        "--placeholder",
+        help="The string to add as a placeholder after the parameter name.",
+        default="FUZZ",
+    )
+    parser.add_argument(
+        "-q",
+        "--quiet",
+        help="Do not print the results to the screen",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-r",
+        "--retries",
+        help="Specify number of retries for 4xx and 5xx errors",
+        default=3,
+    )
 
     args = parser.parse_args()
     return args
@@ -54,59 +89,69 @@ def main():
 
     args = get_args()
 
-    if args.subs == 'True':
+    if args.subs == "True":
         url = f"https://web.archive.org/cdx/search/cdx?url=*.{args.domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
     else:
         url = f"https://web.archive.org/cdx/search/cdx?url={args.domain}/*&output=txt&fl=original&collapse=urlkey&page=/"
-    
+
     retry = True
     retries = 0
     while retry == True and retries <= int(args.retries):
-             response, retry = requester.connector(url)
-             retry = retry
-             retries += 1
+        response, retry = requester.connector(url)
+        retry = retry
+        retries += 1
     if response == False:
-         return
+        return
     response = unquote(response)
-   
-    # for extensions to be excluded 
+
+    # for extensions to be excluded
     black_list = []
     if args.exclude:
-         if "," in args.exclude:
-             black_list = args.exclude.split(",")
-             for i in range(len(black_list)):
-                 black_list[i] = "." + black_list[i]
-         else:
-             black_list.append("." + args.exclude)
-             
-    else: 
-         black_list = [] # for blacklists
+        if "," in args.exclude:
+            black_list = args.exclude.split(",")
+            for i in range(len(black_list)):
+                black_list[i] = "." + black_list[i]
+        else:
+            black_list.append("." + args.exclude)
+
+    else:
+        black_list = []  # for blacklists
     if args.exclude:
-        print(f"\u001b[31m[!] URLS containing these extensions will be excluded from the results   : {black_list}\u001b[0m\n")
-    
-    final_uris = extractor.param_extract(response , args.level , black_list, args.placeholder)
-    save_it.save_func(final_uris , args.output , args.domain)
+        print(
+            f"\u001b[31m[!] URLS containing these extensions will be excluded from the results   : {black_list}\u001b[0m\n"
+        )
+
+    final_uris = extractor.param_extract(
+        response, args.level, black_list, args.placeholder
+    )
+    save_it.save_func(final_uris, args.output, args.domain)
 
     if not args.quiet:
         print("\u001b[32;1m")
-        print('\n'.join(final_uris))
+        print("\n".join(final_uris))
         print("\u001b[0m")
 
     print(f"\n\u001b[32m[+] Total number of retries:  {retries-1}\u001b[31m")
     print(f"\u001b[32m[+] Total unique urls found : {len(final_uris)}\u001b[31m")
     if args.output:
         if "/" in args.output:
-            print(f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36m{args.output}\u001b[31m" )
+            print(
+                f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36m{args.output}\u001b[31m"
+            )
 
         else:
-            print(f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36moutput/{args.output}\u001b[31m" )
+            print(
+                f"\u001b[32m[+] Output is saved here :\u001b[31m \u001b[36moutput/{args.output}\u001b[31m"
+            )
     else:
-        print(f"\u001b[32m[+] Output is saved here   :\u001b[31m \u001b[36moutput/{args.domain}.txt\u001b[31m")
-    print("\n\u001b[31m[!] Total execution time      : %ss\u001b[0m" % str((time.time() - start_time))[:-12])
-
-
+        print(
+            f"\u001b[32m[+] Output is saved here   :\u001b[31m \u001b[36moutput/{args.domain}.txt\u001b[31m"
+        )
+    print(
+        "\n\u001b[31m[!] Total execution time      : %ss\u001b[0m"
+        % str((time.time() - start_time))[:-12]
+    )
 
 
 if __name__ == "__main__":
     main()
-    
