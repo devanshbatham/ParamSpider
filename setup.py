6bc62d9d2637130678825c887db532c6dad2ee60
AliElTop <66541902+dragonked2@users.noreply.github.com>
2023-09-17 02:51:17 +0300

    Update setup.py
    fix decoding error
    By specifying encoding='utf-8' when opening the 'README.md' file, you should be able to read the file without encountering a UnicodeDecodeError.

--- setup.py
+++ setup.py
@@ -17,6 +17,6 @@ setup(
         ]
     },
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown'
 )
