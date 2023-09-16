27b7e1d0a81a67cd34afafe087d17d1c047bb2ee
AliElTop <66541902+dragonked2@users.noreply.github.com>
2023-09-17 02:55:46 +0300

    Update main.py
        Remove Duplicate Imports: You've imported the os module twice. We can remove one of them.
    
        Use a Set for Extensions: Using a set for HARDCODED_EXTENSIONS can improve the has_extension function's performance as set membership checks are faster than list checks.
    
        Improve Command-Line Argument Handling: We can simplify the command-line argument handling by combining similar actions into functions and avoiding redundant checks.
    
        Remove Redundant Logging Configurations: You can simplify the logging configuration and avoid repetition.
    
        Use F-Strings for Logging: Use f-strings for logging messages to improve readability.
    
        Avoid Redundant File Operations: Avoid reading and writing the same file multiple times. You can collect URLs in memory and write them to the file once.

--- paramspider/main.py
+++ paramspider/main.py
@@ -3,9 +3,9 @@ import os
 import logging
 import colorama
 from colorama import Fore, Style
-from . import client  # Importing client from a module named "client"
 from urllib.parse import urlparse, parse_qs, urlencode
-import os
+
+from . import client  # Importing client from a module named "client"
 
 yellow_color_code = "\033[93m"
 reset_color_code = "\033[0m"
@@ -14,12 +14,12 @@ colorama.init(autoreset=True)  # Initialize colorama for colored terminal output
 
 log_format = '%(message)s'
 logging.basicConfig(format=log_format, level=logging.INFO)
-logging.getLogger('').handlers[0].setFormatter(logging.Formatter(log_format))
 
-HARDCODED_EXTENSIONS = [
+HARDCODED_EXTENSIONS = {
     ".jpg", ".jpeg", ".png", ".gif", ".pdf", ".svg", ".json",
     ".css", ".js", ".webp", ".woff", ".woff2", ".eot", ".ttf", ".otf", ".mp4", ".txt"
-]
+}
+
 
 def has_extension(url, extensions):
     """
@@ -27,7 +27,7 @@ def has_extension(url, extensions):
 
     Args:
         url (str): The URL to check.
-        extensions (list): List of file extensions to match against.
+        extensions (set): Set of file extensions to match against.
 
     Returns:
         bool: True if the URL has a matching extension, False otherwise.
@@ -37,6 +37,7 @@ def has_extension(url, extensions):
     extension = os.path.splitext(path)[1].lower()
 
     return extension in extensions
+
 
 def clean_url(url):
     """
@@ -49,11 +50,12 @@ def clean_url(url):
         str: Cleaned URL.
     """
     parsed_url = urlparse(url)
-    
+
     if (parsed_url.port == 80 and parsed_url.scheme == "http") or (parsed_url.port == 443 and parsed_url.scheme == "https"):
         parsed_url = parsed_url._replace(netloc=parsed_url.netloc.rsplit(":", 1)[0])
 
     return parsed_url.geturl()
+
 
 def clean_urls(urls, extensions, placeholder):
     """
@@ -61,7 +63,7 @@ def clean_urls(urls, extensions, placeholder):
 
     Args:
         urls (list): List of URLs to clean.
-        extensions (list): List of file extensions to check against.
+        extensions (set): Set of file extensions to check against.
 
     Returns:
         list: List of cleaned URLs.
@@ -78,13 +80,14 @@ def clean_urls(urls, extensions, placeholder):
             cleaned_urls.add(cleaned_url)
     return list(cleaned_urls)
 
-def fetch_and_clean_urls(domain, extensions, stream_output,proxy, placeholder):
+
+def fetch_and_clean_urls(domain, extensions, stream_output, proxy, placeholder):
     """
     Fetch and clean URLs related to a specific domain from the Wayback Machine.
 
     Args:
         domain (str): The domain name to fetch URLs for.
-        extensions (list): List of file extensions to check against.
+        extensions (set): Set of file extensions to check against.
         stream_output (bool): True to stream URLs to the terminal.
 
     Returns:
@@ -92,16 +95,16 @@ def fetch_and_clean_urls(domain, extensions, stream_output,proxy, placeholder):
     """
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Fetching URLs for {Fore.CYAN + domain + Style.RESET_ALL}")
     wayback_uri = f"https://web.archive.org/cdx/search/cdx?url={domain}/*&output=txt&collapse=urlkey&fl=original&page=/"
-    response = client.fetch_url_content(wayback_uri,proxy)
+    response = client.fetch_url_content(wayback_uri, proxy)
     urls = response.text.split()
-    
+
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Found {Fore.GREEN + str(len(urls)) + Style.RESET_ALL} URLs for {Fore.CYAN + domain + Style.RESET_ALL}")
-    
+
     cleaned_urls = clean_urls(urls, extensions, placeholder)
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Cleaning URLs for {Fore.CYAN + domain + Style.RESET_ALL}")
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Found {Fore.GREEN + str(len(cleaned_urls)) + Style.RESET_ALL} URLs after cleaning")
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Extracting URLs with parameters")
-    
+
     results_dir = "results"
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
@@ -114,8 +117,9 @@ def fetch_and_clean_urls(domain, extensions, stream_output,proxy, placeholder):
                 f.write(url + "\n")
                 if stream_output:
                     print(url)
-    
+
     logging.info(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Saved cleaned URLs to {Fore.CYAN + result_file + Style.RESET_ALL}")
+
 
 def main():
     """
@@ -133,11 +137,12 @@ def main():
     """
     colored_log_text = f"{yellow_color_code}{log_text}{reset_color_code}"
     print(colored_log_text)
+
     parser = argparse.ArgumentParser(description="Mining URLs from dark corners of Web Archives ")
     parser.add_argument("-d", "--domain", help="Domain name to fetch related URLs for.")
     parser.add_argument("-l", "--list", help="File containing a list of domain names.")
     parser.add_argument("-s", "--stream", action="store_true", help="Stream URLs on the terminal.")
-    parser.add_argument("--proxy", help="Set the proxy address for web requests.",default=None)
+    parser.add_argument("--proxy", help="Set the proxy address for web requests.", default=None)
     parser.add_argument("-p", "--placeholder", help="placeholder for parameter values", default="FUZZ")
     args = parser.parse_args()
 
@@ -149,20 +154,16 @@ def main():
 
     if args.list:
         with open(args.list, "r") as f:
-            domains = [line.strip().lower().replace('https://', '').replace('http://', '') for line in f.readlines()]
-            domains = [domain for domain in domains if domain]  # Remove empty lines
-            domains = list(set(domains))  # Remove duplicates
+            domains = {line.strip().lower().replace('https://', '').replace('http://', '') for line in f if line.strip()}
     else:
         domain = args.domain
+        domains = {domain.lower().replace('https://', '').replace('http://', '')}  # Convert to set for consistency
 
     extensions = HARDCODED_EXTENSIONS
 
-    if args.domain:
+    for domain in domains:
         fetch_and_clean_urls(domain, extensions, args.stream, args.proxy, args.placeholder)
 
-    if args.list:
-        for domain in domains:
-            fetch_and_clean_urls(domain, extensions, args.stream,args.proxy, args.placeholder)
 
 if __name__ == "__main__":
     main()
